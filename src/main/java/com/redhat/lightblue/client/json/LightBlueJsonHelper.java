a924471e2cd06060b3c54616036171971f65a768
Joshua Junqueira <jjunquei@redhat.com>
2014-07-11 13:44:22 -0400

    Added a method stub for creating put request

--- src/main/java/com/redhat/lightblue/client/json/LightBlueJsonHelper.java
+++ src/main/java/com/redhat/lightblue/client/json/LightBlueJsonHelper.java
@@ -15,4 +15,7 @@ public interface LightBlueJsonHelper {
 	 * @return A map representation of a JSON string or null if the json could not be parsed
 	 */
 	public Map<String,Object> getJsonMap(String json);
+	
+	public String createEntityPutRequestJson(String entity,String version,Map<String,Object> data);
+	
 }
