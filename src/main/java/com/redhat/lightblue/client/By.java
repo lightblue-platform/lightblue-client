6625f55e6f099e8b0d1b1a98f5ed4ab29d47367b
Benjamin Miller <bmiller@redhat.com>
2014-07-11 12:52:56 -0400

    Make list in By class private

--- src/main/java/com/redhat/lightblue/client/By.java
+++ src/main/java/com/redhat/lightblue/client/By.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class By {
-    List<SearchCriteria> criteria;
+    private List<SearchCriteria> criteria;
 
     public By () {
         criteria = new ArrayList<SearchCriteria>();
