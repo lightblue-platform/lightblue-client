1333d3a0e0a9fc923208f08d6108aceb4beb9959
Michael White <miwhite@redhat.com>
2014-10-10 09:30:06 -0400

    Creating skeleton interfaces.

---
+++ src/main/java/com/redhat/lightblue/client/expression/Expression.java
@@ -0,0 +1,10 @@
+package com.redhat.lightblue.client.expression;
+
+/**
+ * created by Michael White 10/09/2014
+ */
+
+public interface Expression {
+    
+    String toJson();
+}
---
+++ src/main/java/com/redhat/lightblue/client/projection/Projection.java
@@ -0,0 +1,10 @@
+package com.redhat.lightblue.client.projection;
+
+/**
+ * created by Michael White 10/09/2014
+ */
+
+public interface Projection {
+    
+    String toJson();
+}
