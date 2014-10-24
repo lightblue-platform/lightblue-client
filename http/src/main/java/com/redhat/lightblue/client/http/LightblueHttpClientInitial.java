1ff5de13964ad466948404eb155593c052507699
Derek Haynes <dhaynes@redhat.com>
2014-10-24 10:07:03 -0400

    Remove unneeded classes

--- http/src/main/java/com/redhat/lightblue/client/http/LightblueHttpClientInitial.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.lightblue.client.http;
-
-import org.apache.http.impl.client.CloseableHttpClient;
-
-public interface LightblueHttpClientInitial {
-
-	public abstract CloseableHttpClient getClient();
-
-}
