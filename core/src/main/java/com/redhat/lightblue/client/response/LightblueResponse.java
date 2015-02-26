99364fabc9c847fd07e42b5da9726df076de4d39
Dennis Crissman <dcrissma@redhat.com>
2015-02-26 10:43:16 -0500

    add error when unable to parse response

--- core/src/main/java/com/redhat/lightblue/client/response/LightblueResponse.java
+++ core/src/main/java/com/redhat/lightblue/client/response/LightblueResponse.java
@@ -7,37 +7,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class LightblueResponse {
 
-	private String text;
-	private JsonNode json;
-	
-	public LightblueResponse() {
-		
-	}
-	
-	public LightblueResponse(String responseText) {
-		this.text = responseText;
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-	    json = mapper.readTree(responseText);
-    } catch (IOException e) {
-	    throw new RuntimeException(e);
+    private String text;
+    private JsonNode json;
+
+    public LightblueResponse() {
+
     }
-	}
-	
-	public String getText() {
-		return text;
-	}
-	
-	public void setText(String text) {
-		this.text = text;
-	}
-	
-	public JsonNode getJson() {
-		return json;
-	}
-	
-	public void setJson(JsonNode json) {
-		this.json = json;
-	}
-	
+
+    public LightblueResponse(String responseText) {
+        this.text = responseText;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            json = mapper.readTree(responseText);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to parse response: " + responseText, e);
+        }
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public JsonNode getJson() {
+        return json;
+    }
+
+    public void setJson(JsonNode json) {
+        this.json = json;
+    }
+
 }
