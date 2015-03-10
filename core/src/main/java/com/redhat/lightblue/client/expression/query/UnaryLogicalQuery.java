de64c661657d69758205be09a7fdb8c0d15d9b9e
Dennis Crissman <dcrissma@redhat.com>
2015-03-10 11:08:13 -0400

    add helper method for 'not' operations

--- core/src/main/java/com/redhat/lightblue/client/expression/query/UnaryLogicalQuery.java
+++ core/src/main/java/com/redhat/lightblue/client/expression/query/UnaryLogicalQuery.java
@@ -2,33 +2,35 @@ package com.redhat.lightblue.client.expression.query;
 
 import com.redhat.lightblue.client.enums.UnaryOperation;
 
-
-import java.lang.StringBuffer;
-
 /**
  * created by Michael White 10/10/2014
  */
 
 public class UnaryLogicalQuery implements Query {
-    
-    private UnaryOperation operation;
-    private Query queryExpression;
-    
+
+    private final UnaryOperation operation;
+    private final Query queryExpression;
+
     // used for NOT
     public UnaryLogicalQuery(UnaryOperation operation, Query queryExpression) {
         this.operation = operation;
         this.queryExpression = queryExpression;
     }
-    
+
+    @Override
     public String toJson() throws IllegalArgumentException {
         StringBuffer builder = new StringBuffer();
-        
+
         builder.append("{\"");
         builder.append( this.operation.toString() );
         builder.append("\":");
         builder.append( this.queryExpression.toJson() );
         builder.append("}");
-        
+
         return builder.toString();
+    }
+
+    public static UnaryLogicalQuery not(Query queryExpression){
+        return new UnaryLogicalQuery(UnaryOperation.NOT, queryExpression);
     }
 }
