499e853408c49e45a81afcef318cb6a5d448d220
Marek Paterczyk <mpatercz@redhat.com>
2015-03-19 12:35:05 -0400

    Fixed ValueQuery expressions to allow values with spaces. Added some expression validation logic.

--- core/src/main/java/com/redhat/lightblue/client/expression/query/ValueQuery.java
+++ core/src/main/java/com/redhat/lightblue/client/expression/query/ValueQuery.java
@@ -1,20 +1,45 @@
 package com.redhat.lightblue.client.expression.query;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.redhat.lightblue.client.enums.ExpressionOperation;
 
 /**
  * Created by bmiller on 10/10/14.
+ *
+ * @author bmiller
+ * @author mpatercz
  */
 public class ValueQuery implements Query {
     private String field;
     private String rValue;
     private String operator;
 
+    public static final Pattern expressionPattern = Pattern.compile("(\\w+)\\s*(\\S+)\\s*(.+)$");
+
+    // binary_comparison_operator := "=" | "!=" | "<" | ">" | "<=" | ">=" |
+    //        "$eq" | "$neq" | "$lt" | "$gt" | "$lte" | "$gte
+    public static final String[] allowedOperators = new String[]{"=", "!=", "<", ">", "<=", ">=", "$eq", "$neq", "$lt", "$lte", "$gte" };
+    public static final Set<String> allowedOperatorsSet = new HashSet<String>(Arrays.asList(allowedOperators));
+
     public ValueQuery(String expression) {
-        String[] parts = expression.split("\\s");
-        field = parts[0];
-        operator = parts[1];
-        rValue = parts[2];
+        Matcher m = expressionPattern.matcher(expression);
+        if (m.find()) {
+            field = m.group(1);
+            operator = m.group(2);
+            rValue = m.group(3);
+
+            if (!allowedOperatorsSet.contains(operator)) {
+                throw new IllegalArgumentException(operator+" operator is not allowed. Allowed options are: "+allowedOperatorsSet.toString());
+            }
+        }
+        else {
+            throw new IllegalArgumentException("'"+expression +"' is incorrect");
+        }
     }
 
     public ValueQuery(String field, ExpressionOperation operation, String rValue) {
@@ -37,6 +62,14 @@ public class ValueQuery implements Query {
         return toJson();
     }
 
+    /**
+     * Expression must follow this pattern: field op value, where field cannot
+     * contain any whitespace characters (value can) and op is one of the following
+     * operators: {"=", "!=", "<", ">", "<=", ">=", "$eq", "$neq", "$lt", "$lte", "$gte" }
+     *
+     * @param expression
+     * @return
+     */
     public static ValueQuery withValue(String expression){
         return new ValueQuery(expression);
     }
--- core/src/test/java/com/redhat/lightblue/client/expression/query/ValueQueryTest.java
+++ core/src/test/java/com/redhat/lightblue/client/expression/query/ValueQueryTest.java
@@ -1,9 +1,11 @@
 package com.redhat.lightblue.client.expression.query;
 
-import com.redhat.lightblue.client.enums.ExpressionOperation;
 import org.json.JSONException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
+
+import com.redhat.lightblue.client.enums.ExpressionOperation;
 
 /**
  * Created by bmiller on 10/10/14.
@@ -32,5 +34,26 @@ public class ValueQueryTest {
         ValueQuery expression = new ValueQuery("field1 = value1");
 
         JSONAssert.assertEquals(expression.toString(), expression.toJson(), false);
+    }
+
+    @Test
+    public void testValueWithWhiteSpace() throws JSONException {
+        ValueQuery expression = new ValueQuery("field1 = Red Hat Enterprise Linux");
+
+        String expectedJson = "{\"field\":\"field1\",\"op\":\"=\",\"rvalue\":\"Red Hat Enterprise Linux\"}";
+
+        JSONAssert.assertEquals(expectedJson, expression.toJson(), false);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testInvalidExpression() {
+        new ValueQuery("fie ld1 = Red Hat Enterprise Linux");
+        Assert.fail();
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testInvalidOperator() {
+        new ValueQuery("field1 ~= Red Hat Enterprise Linux");
+        Assert.fail();
     }
 }
