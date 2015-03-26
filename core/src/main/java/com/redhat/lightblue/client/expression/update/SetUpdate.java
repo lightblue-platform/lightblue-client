1c10636b4e5347db2cc8448697a44cffd20ad832
Naveen Malik <nmalik@redhat.com>
2015-03-26 09:08:41 -0400

    Fixes 'Impossible downcast of toArray() result' issue from sonar

--- core/src/main/java/com/redhat/lightblue/client/expression/update/SetUpdate.java
+++ core/src/main/java/com/redhat/lightblue/client/expression/update/SetUpdate.java
@@ -7,26 +7,26 @@ import com.redhat.lightblue.client.util.JSON;
 /**
  * created by Michael White 10/10/2014
  */
-
 public class SetUpdate implements Update {
-
     PathValuePair[] pathValuePairs;
 
-    public SetUpdate(PathValuePair... statements ){
+    public SetUpdate(PathValuePair... statements) {
         pathValuePairs = statements;
     }
 
-    public SetUpdate(Collection<PathValuePair> statements ){
-        pathValuePairs = (PathValuePair[])statements.toArray();
+    public SetUpdate(Collection<PathValuePair> statements) {
+        if (statements != null) {
+            pathValuePairs = statements.toArray(new PathValuePair[statements.size()]);
+        }
     }
 
     @Override
     public String toJson() {
         StringBuilder json = new StringBuilder("{");
-        json.append(JSON.toJson("$set") + ":{");
-        for(int index=0; index<pathValuePairs.length;index++){
+        json.append(JSON.toJson("$set")).append(":{");
+        for (int index = 0; index < pathValuePairs.length; index++) {
             json.append(pathValuePairs[index].toJson());
-            if( ( this.pathValuePairs.length - index ) > 1 ){
+            if ((this.pathValuePairs.length - index) > 1) {
                 json.append(", ");
             }
         }
