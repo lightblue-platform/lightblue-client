c4c346bfd724cce9aaa5c503ae05f603ecdbfe42
Jared Blashka <jblashka@redhat.com>
2015-03-30 15:53:33 -0400

    Adding nary field relational expressions to FieldQuery

--- core/src/test/java/com/redhat/lightblue/client/expression/query/FieldQueryTest.java
+++ core/src/test/java/com/redhat/lightblue/client/expression/query/FieldQueryTest.java
@@ -1,6 +1,7 @@
 package com.redhat.lightblue.client.expression.query;
 
 import com.redhat.lightblue.client.enums.ExpressionOperation;
+import com.redhat.lightblue.client.enums.NaryExpressionOperation;
 import org.json.JSONException;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -15,6 +16,15 @@ public class FieldQueryTest {
         FieldQuery expression = new FieldQuery("field1", ExpressionOperation.EQUALS, "field2");
 
         String expectedJson = "{\"field\":\"field1\",\"op\":\"=\",\"rfield\":\"field2\"}";
+
+        JSONAssert.assertEquals(expectedJson, expression.toJson(), false);
+    }
+
+    @Test
+    public void testNaryFieldComparison() throws JSONException {
+        FieldQuery expression = new FieldQuery("field1", NaryExpressionOperation.IN, "field2");
+
+        String expectedJson = "{\"field\":\"field1\",\"op\":\"$in\",\"rfield\":\"field2\"}";
 
         JSONAssert.assertEquals(expectedJson, expression.toJson(), false);
     }
