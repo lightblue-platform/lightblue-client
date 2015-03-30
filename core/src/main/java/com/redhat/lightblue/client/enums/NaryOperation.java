53e3468c6429d64e807385adfb0644ef96b27c7a
Dennis Crissman <dcrissma@redhat.com>
2015-03-30 12:45:06 -0400

    ensure consistency of enums

--- core/src/main/java/com/redhat/lightblue/client/enums/NaryOperation.java
+++ core/src/main/java/com/redhat/lightblue/client/enums/NaryOperation.java
@@ -1,22 +1,44 @@
 package com.redhat.lightblue.client.enums;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public enum NaryOperation {
-    AND, OR, ALL, ANY;
+    AND("$and"),
+    OR("$or"),
+    ALL("$all"),
+    ANY("$any");
+
+    private String operator;
+
+    public String getOperator() {
+        return operator;
+    }
+
+    private NaryOperation(String expression) {
+        this.operator = expression;
+    }
+
+    public static Set<String> getOperators() {
+        Set<String> set = new HashSet<String>();
+        for (NaryOperation exp : values()) {
+            set.add(exp.getOperator());
+        }
+        return set;
+    }
+
+    public static boolean contains(String operator) {
+        for (NaryOperation e : values()) {
+            if (e.getOperator().equalsIgnoreCase(operator)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
-        switch (this) {
-            case AND:
-                return "$and";
-            case OR:
-                return "$or";
-            case ALL:
-                return "$all";
-            case ANY:
-                return "$any";
-            default:
-                throw new IllegalArgumentException();
-        }
+        return getOperator();
     }
 
 }
