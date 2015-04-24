b4b2d15e3b7b131ee9dbeccd63ceeebb0d756022
Dennis Crissman <dcrissma@redhat.com>
2015-04-24 15:03:21 -0400

    clean up based on recent changes

--- lightblue-client-test/src/main/java/com/redhat/lightblue/client/test/request/DataFindRequestStub.java
+++ lightblue-client-test/src/main/java/com/redhat/lightblue/client/test/request/DataFindRequestStub.java
@@ -6,13 +6,15 @@ import com.redhat.lightblue.client.request.data.DataFindRequest;
  * Created by jblashka on 11/3/14.
  */
 public class DataFindRequestStub extends DataFindRequest {
-    private String body;
-    public DataFindRequestStub(String entityName, String entityVersion, String body){
-        setEntityName(entityName);
-        setEntityVersion(entityVersion);
+    private final String body;
+
+    public DataFindRequestStub(String entityName, String entityVersion, String body) {
+        super(entityName, entityVersion);
         this.body = body;
     }
-    public String getBody(){
+
+    @Override
+    public String getBody() {
         return body;
     }
 
--- lightblue-client-test/src/main/java/com/redhat/lightblue/client/test/request/DataUpdateRequestStub.java
+++ lightblue-client-test/src/main/java/com/redhat/lightblue/client/test/request/DataUpdateRequestStub.java
@@ -6,13 +6,16 @@ import com.redhat.lightblue.client.request.data.DataUpdateRequest;
  * Created by jblashka on 10/20/14.
  */
 public class DataUpdateRequestStub extends DataUpdateRequest {
-    private String body;
-    public DataUpdateRequestStub(String entityName, String entityVersion, String body){
-        setEntityName(entityName);
-        setEntityVersion(entityVersion);
+    private final String body;
+
+    public DataUpdateRequestStub(String entityName, String entityVersion, String body) {
+        super(entityName, entityVersion);
         this.body = body;
     }
-    public String getBody(){
+
+    @Override
+    public String getBody() {
         return body;
     }
+
 }
