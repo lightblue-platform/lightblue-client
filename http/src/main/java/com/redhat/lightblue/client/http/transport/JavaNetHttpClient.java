18aa12bc94afa4f77377333ba6c168c934b74684
Alec Henninger <ahenning@redhat.com>
2015-05-16 13:38:31 -0400

    Correctly handle error responses in java.net client

--- http/src/main/java/com/redhat/lightblue/client/http/transport/JavaNetHttpClient.java
+++ http/src/main/java/com/redhat/lightblue/client/http/transport/JavaNetHttpClient.java
@@ -1,7 +1,6 @@
 package com.redhat.lightblue.client.http.transport;
 
 import com.redhat.lightblue.client.LightblueClientConfiguration;
-import com.redhat.lightblue.client.http.HttpMethod;
 import com.redhat.lightblue.client.http.auth.SslSocketFactories;
 import com.redhat.lightblue.client.request.LightblueRequest;
 
@@ -19,7 +18,20 @@ import java.nio.charset.Charset;
 import java.security.GeneralSecurityException;
 import java.util.Objects;
 
+/**
+ * An {@link HttpClient} which uses vanilla java.net classes to make the request. Recommended to use
+ * in a servlet environment which is already multi-threaded by virtue of the application server.
+ * This class is thread safe, so you should use an application-scoped
+ * {@link com.redhat.lightblue.client.http.LightblueHttpClient} backed by an instance of this class,
+ * which is the default behavior if you have not passed in a different {@code HttpClient}.
+ *
+ * <p>This implementation takes advantage of HTTP persistent connections as per:
+ * <a href="http://docs.oracle.com/javase/7/docs/technotes/guides/net/http-keepalive.html">http://docs.oracle.com/javase/7/docs/technotes/guides/net/http-keepalive.html</a>.
+ * Sockets are left open to be reused after each request per Java SDK semantics.
+ */
 public class JavaNetHttpClient implements HttpClient {
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+
     private final ConnectionFactory connectionFactory;
     private final SSLSocketFactory sslSocketFactory;
 
@@ -58,7 +70,8 @@ public class JavaNetHttpClient implements HttpClient {
 
     @Override
     public String executeRequest(LightblueRequest request, String baseUri) throws IOException {
-        HttpURLConnection connection = connectionFactory.openConnection(request.getRestURI(baseUri));
+        String url = request.getRestURI(baseUri);
+        HttpURLConnection connection = connectionFactory.openConnection(url);
 
         if (connection instanceof HttpsURLConnection) {
             HttpsURLConnection httpsUrlConnection = (HttpsURLConnection) connection;
@@ -68,31 +81,75 @@ public class JavaNetHttpClient implements HttpClient {
             }
         }
 
-        HttpMethod method = request.getHttpMethod();
         String body = request.getBody();
-        int length = body.length();
-
-        connection.setRequestMethod(method.toString());
-
         if (StringUtils.isNotBlank(body)) {
-            connection.setDoOutput(true);
-            connection.setFixedLengthStreamingMode(length);
-            connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-            connection.setRequestProperty("Content-Length", Integer.toString(length));
-
-            try (OutputStream requestStream = connection.getOutputStream()) {
-                requestStream.write(body.getBytes(Charset.forName("UTF-8")));
-            }
+            addRequestBodyToConnection(body, connection);
         }
 
-        InputStream responseStream = connection.getInputStream();
+        connection.setRequestMethod(request.getHttpMethod().toString());
 
-        return IOUtils.toString(responseStream, Charset.forName("UTF-8"));
+        return response(connection);
     }
 
     @Override
     public void close() throws IOException {
-        // Do nothing
+        // Nothing to do
+    }
+
+    private void addRequestBodyToConnection(String body, HttpURLConnection connection)
+            throws IOException {
+        int length = body.length();
+
+        connection.setDoOutput(true);
+        connection.setFixedLengthStreamingMode(length);
+
+        connection.setRequestProperty("Content-Length", Integer.toString(length));
+        connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+        try (OutputStream requestStream = connection.getOutputStream()) {
+            requestStream.write(body.getBytes(UTF_8));
+        }
+    }
+
+    /**
+     * Parses response, whether or not the request was successful, if possible. Reads entire input
+     * stream and closes in order to so the socket knows it is finished and may be put back into a
+     * pool for reuse.
+     */
+    private String response(HttpURLConnection connection) throws IOException {
+        try (InputStream responseStream = connection.getInputStream()) {
+            return readResponseStream(responseStream, connection);
+        } catch (IOException e) {
+            try (InputStream errorResponseStream = connection.getErrorStream()) {
+                if (errorResponseStream == null) {
+                    // TODO: May want to start returning status code / error line.
+                    // In the meantime I don't think lightblue should ever error without a response.
+                    return "";
+                }
+
+                return readResponseStream(errorResponseStream, connection);
+            }
+        }
+    }
+
+    /**
+     * Tries to efficiently allocate the response string if the "Content-Length" header is set.
+     */
+    private String readResponseStream(InputStream responseStream, HttpURLConnection connection)
+            throws IOException {
+        int contentLength = connection.getContentLength();
+
+        if (contentLength == 0) {
+            return "";
+        }
+
+        if (contentLength > 0) {
+            byte[] responseBytes = new byte[contentLength];
+            IOUtils.readFully(responseStream, responseBytes);
+            return new String(responseBytes, UTF_8);
+        }
+
+        return IOUtils.toString(responseStream, UTF_8);
     }
 
     public interface ConnectionFactory {
---
+++ http/src/test/java/com/redhat/lightblue/client/http/transport/JavaNetHttpClientIntegrationTest.java
@@ -0,0 +1,67 @@
+package com.redhat.lightblue.client.http.transport;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.redhat.lightblue.client.http.HttpMethod;
+import com.redhat.lightblue.client.http.testing.doubles.FakeLightblueRequest;
+import com.redhat.lightblue.client.request.LightblueRequest;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+
+@RunWith(JUnit4.class)
+public class JavaNetHttpClientIntegrationTest {
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule();
+
+    private JavaNetHttpClient client = new JavaNetHttpClient();
+
+    @Test
+    public void shouldReturnResponseBodyOfSuccessfulRequest() throws IOException {
+        wireMockRule.stubFor(get(urlMatching(".*"))
+                .willReturn(aResponse().withBody("The body").withStatus(200)));
+
+        LightblueRequest request = new FakeLightblueRequest("", HttpMethod.GET, "/");
+
+        String response = client.executeRequest(request, wireMockUrl());
+
+        assertThat(response, is("The body"));
+    }
+
+    @Test
+    public void shouldReturnResponseBodyOfUnsuccessfulRequest() throws IOException {
+        wireMockRule.stubFor(get(urlMatching(".*"))
+                .willReturn(aResponse().withBody("The body").withStatus(500)));
+
+        LightblueRequest request = new FakeLightblueRequest("", HttpMethod.GET, "/");
+
+        String response = client.executeRequest(request, wireMockUrl());
+
+        assertThat(response, is("The body"));
+    }
+
+    @Test
+    public void shouldReturnEmptyStringOfUnsuccessfulRequestWithNoResponseBody() throws IOException {
+        wireMockRule.stubFor(get(urlMatching(".*"))
+                .willReturn(aResponse().withStatus(500)));
+
+        LightblueRequest request = new FakeLightblueRequest("", HttpMethod.GET, "/");
+
+        String response = client.executeRequest(request, wireMockUrl());
+
+        assertThat(response, is(""));
+    }
+
+    private String wireMockUrl() {
+        return "http://localhost:" + wireMockRule.port();
+    }
+}
