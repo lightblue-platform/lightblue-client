b2504ab82c865ce606b61e90d7efc75512956e26
Vritant Jain <vrjain@redhat.com>
2015-07-28 15:56:02 -0400

    update MetadataUpdateSchemaStatusRequest - make it work

--- core/src/main/java/com/redhat/lightblue/client/request/metadata/MetadataUpdateSchemaStatusRequest.java
+++ core/src/main/java/com/redhat/lightblue/client/request/metadata/MetadataUpdateSchemaStatusRequest.java
@@ -1,26 +1,34 @@
 package com.redhat.lightblue.client.request.metadata;
 
+import com.redhat.lightblue.client.enums.MetadataStatus;
 import com.redhat.lightblue.client.http.HttpMethod;
 import com.redhat.lightblue.client.request.AbstractLightblueMetadataRequest;
 
 public class MetadataUpdateSchemaStatusRequest extends AbstractLightblueMetadataRequest {
 
+    private MetadataStatus status;
+
     public MetadataUpdateSchemaStatusRequest() {
         super();
     }
 
-    public MetadataUpdateSchemaStatusRequest(String entityName, String entityVersion) {
+    public MetadataUpdateSchemaStatusRequest(String entityName, String entityVersion, MetadataStatus status) {
         super(entityName, entityVersion);
+        this.status = status;
     }
 
     @Override
     public String getOperationPathParam() {
-        return "";
+        return status.getStatus();
     }
 
     @Override
     public HttpMethod getHttpMethod() {
         return HttpMethod.PUT;
+    }
+
+    public void setStatus(MetadataStatus status) {
+        this.status = status;
     }
 
 }
