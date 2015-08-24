c33926f9f8e11aa687e60afd7ab9e72f7eaefd49
Dennis Crissman <dcrissma@redhat.com>
2015-08-24 16:03:41 -0400

    use ThreadLocal instead of clone

--- core/src/main/java/com/redhat/lightblue/client/util/ClientConstants.java
+++ core/src/main/java/com/redhat/lightblue/client/util/ClientConstants.java
@@ -4,19 +4,18 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public final class ClientConstants {
-    private static final DateFormat DATE_FORMAT;
-    private static final String DATE_FORMAT_STR = "yyyyMMdd'T'HH:mm:ss.SSSZ";
-
-    static {
-        DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT_STR);
-    }
+    private static final ThreadLocal<DateFormat> DATE_FORMATS = new ThreadLocal<>();
+    public static final String DATE_FORMAT_STR = "yyyyMMdd'T'HH:mm:ss.SSSZ";
 
     /**
-     * Returns a DateFormat instance using the DATE_FORMAT_STR in GMT. Clone of
-     * the static internal variable, because SimpleDateFormat is not thread safe
+     * Returns a DateFormat instance using the DATE_FORMAT_STR. Uses ThreadLocal
+     * under the hood, because SimpleDateFormat is not thread safe
      */
     public static DateFormat getDateFormat() {
-        return (DateFormat) DATE_FORMAT.clone();
+        if (DATE_FORMATS.get() == null) {
+            DATE_FORMATS.set(new SimpleDateFormat(DATE_FORMAT_STR));
+        }
+        return DATE_FORMATS.get();
     }
 
     private ClientConstants() {}
