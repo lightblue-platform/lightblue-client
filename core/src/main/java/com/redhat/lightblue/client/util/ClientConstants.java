a9da5f24a0c1ba0978594941a9c506785443673a
Dennis Crissman <dcrissma@redhat.com>
2015-08-25 08:37:53 -0400

    Use ThreadLocal and clone for the lightblue date format

--- core/src/main/java/com/redhat/lightblue/client/util/ClientConstants.java
+++ core/src/main/java/com/redhat/lightblue/client/util/ClientConstants.java
@@ -4,16 +4,23 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public final class ClientConstants {
+
+    /** String representation of the {@link DateFormat} used by lightblue. */
+    public static final String LIGHTBLUE_DATE_FORMAT_STR = "yyyyMMdd'T'HH:mm:ss.SSSZ";
+
+    /** Contains the lightblue {@link DateFormat} for each Thread. */
     private static final ThreadLocal<DateFormat> DATE_FORMATS = new ThreadLocal<>();
-    public static final String DATE_FORMAT_STR = "yyyyMMdd'T'HH:mm:ss.SSSZ";
+    /** It is faster to clone than to create new {@link DateFormat} instances.
+     * This is the base instance from which others are cloned. */
+    private static final DateFormat BASE_DATE_FORMAT = new SimpleDateFormat(LIGHTBLUE_DATE_FORMAT_STR);
 
     /**
-     * Returns a DateFormat instance using the DATE_FORMAT_STR. Uses ThreadLocal
-     * under the hood, because SimpleDateFormat is not thread safe
+     * Returns a {@link DateFormat} instance using the {@link #LIGHTBLUE_DATE_FORMAT_STR}. Uses {@link ThreadLocal}
+     * under the hood, because {@link SimpleDateFormat} is not thread safe
      */
     public static DateFormat getDateFormat() {
         if (DATE_FORMATS.get() == null) {
-            DATE_FORMATS.set(new SimpleDateFormat(DATE_FORMAT_STR));
+            DATE_FORMATS.set((DateFormat) BASE_DATE_FORMAT.clone());
         }
         return DATE_FORMATS.get();
     }
