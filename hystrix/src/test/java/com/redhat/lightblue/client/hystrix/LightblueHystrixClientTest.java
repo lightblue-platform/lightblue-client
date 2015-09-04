8f93e7482a823c251551e864ec11eacda99a91bc
bvulaj <bvulaj@redhat.com>
2015-09-04 11:50:17 -0400

    Updated HystrixClient with bulk operations Also changed from overload to standalone method in httpCLient Added some basic tests for hystrix client

--- hystrix/src/test/java/com/redhat/lightblue/client/hystrix/LightblueHystrixClientTest.java
+++ hystrix/src/test/java/com/redhat/lightblue/client/hystrix/LightblueHystrixClientTest.java
@@ -12,9 +12,11 @@ import org.junit.Test;
 
 import com.redhat.lightblue.client.LightblueClient;
 import com.redhat.lightblue.client.Locking;
+import com.redhat.lightblue.client.request.AbstractBulkLightblueRequest;
 import com.redhat.lightblue.client.request.AbstractLightblueDataRequest;
 import com.redhat.lightblue.client.request.AbstractLightblueMetadataRequest;
 import com.redhat.lightblue.client.request.LightblueRequest;
+import com.redhat.lightblue.client.response.BulkLightblueResponse;
 import com.redhat.lightblue.client.response.LightblueException;
 import com.redhat.lightblue.client.response.LightblueResponse;
 
@@ -23,83 +25,106 @@ import com.redhat.lightblue.client.response.LightblueResponse;
  * @author nmalik
  */
 public class LightblueHystrixClientTest {
-    /**
-     * Mock LightblueClient for testing.
-     */
-    private class TestLightblueClient implements LightblueClient {
-        boolean metadata = false;
-        boolean data = false;
-        boolean dataType = false;
+	/**
+	 * Mock LightblueClient for testing.
+	 */
+	private class TestLightblueClient implements LightblueClient {
+		boolean metadata = false;
+		boolean data = false;
+		boolean dataType = false;
+		boolean dataBulk = false;
 
-        @Override
-        public LightblueResponse metadata(LightblueRequest lightblueRequest) {
-            metadata = true;
-            return null;
-        }
+		@Override
+		public LightblueResponse metadata(LightblueRequest lightblueRequest) {
+			metadata = true;
+			return null;
+		}
 
-        @Override
-        public LightblueResponse data(LightblueRequest lightblueRequest) {
-            data = true;
-            return null;
-        }
+		@Override
+		public LightblueResponse data(LightblueRequest lightblueRequest) {
+			data = true;
+			return null;
+		}
 
-        @Override
-        public <T> T data(AbstractLightblueDataRequest lightblueRequest, Class<T> type) throws LightblueException {
-            dataType = true;
-            return null;
-        }
+		@Override
+		public <T> T data(AbstractLightblueDataRequest lightblueRequest, Class<T> type) throws LightblueException {
+			dataType = true;
+			return null;
+		}
 
-        @Override
-        public Locking getLocking(String domain) {
-            return null;
-        }
-    }
+		@Override
+		public BulkLightblueResponse dataBulk(AbstractBulkLightblueRequest<AbstractLightblueDataRequest> bulkLightblueRequest) throws LightblueException {
+			dataBulk = true;
+			return null;
+		}
 
-    @Test
-    public void metadata() {
-        TestLightblueClient client = new TestLightblueClient();
-        LightblueHystrixClient hystrixClient = new LightblueHystrixClient(client, "group", "command");
+		@Override
+		public Locking getLocking(String domain) {
+			return null;
+		}
+	}
 
-        Assert.assertFalse(client.metadata);
-        Assert.assertFalse(client.data);
-        Assert.assertFalse(client.dataType);
+	@Test
+	public void metadata() {
+		TestLightblueClient client = new TestLightblueClient();
+		LightblueHystrixClient hystrixClient = new LightblueHystrixClient(client, "group", "command");
 
-        hystrixClient.metadata(null);
+		Assert.assertFalse(client.metadata);
+		Assert.assertFalse(client.data);
+		Assert.assertFalse(client.dataType);
 
-        Assert.assertTrue(client.metadata);
-        Assert.assertFalse(client.data);
-        Assert.assertFalse(client.dataType);
-    }
+		hystrixClient.metadata(null);
 
-    @Test
-    public void data() {
-        TestLightblueClient client = new TestLightblueClient();
-        LightblueHystrixClient hystrixClient = new LightblueHystrixClient(client, "group", "command");
+		Assert.assertTrue(client.metadata);
+		Assert.assertFalse(client.data);
+		Assert.assertFalse(client.dataType);
+	}
 
-        Assert.assertFalse(client.metadata);
-        Assert.assertFalse(client.data);
-        Assert.assertFalse(client.dataType);
+	@Test
+	public void data() {
+		TestLightblueClient client = new TestLightblueClient();
+		LightblueHystrixClient hystrixClient = new LightblueHystrixClient(client, "group", "command");
 
-        hystrixClient.data(null);
+		Assert.assertFalse(client.metadata);
+		Assert.assertFalse(client.data);
+		Assert.assertFalse(client.dataType);
 
-        Assert.assertFalse(client.metadata);
-        Assert.assertTrue(client.data);
-        Assert.assertFalse(client.dataType);
-    }
+		hystrixClient.data(null);
 
-    @Test
-    public void dataType() throws LightblueException {
-        TestLightblueClient client = new TestLightblueClient();
-        LightblueHystrixClient hystrixClient = new LightblueHystrixClient(client, "group", "command");
+		Assert.assertFalse(client.metadata);
+		Assert.assertTrue(client.data);
+		Assert.assertFalse(client.dataType);
+	}
+	
+	@Test
+	public void dataBulk() throws LightblueException {
+		TestLightblueClient client = new TestLightblueClient();
+		LightblueHystrixClient hystrixClient = new LightblueHystrixClient(client, "group", "command");
 
-        Assert.assertFalse(client.metadata);
-        Assert.assertFalse(client.data);
-        Assert.assertFalse(client.dataType);
+		Assert.assertFalse(client.metadata);
+		Assert.assertFalse(client.data);
+		Assert.assertFalse(client.dataType);
 
-        hystrixClient.data(null, Object.class);
+		hystrixClient.dataBulk(null);
 
-        Assert.assertFalse(client.metadata);
-        Assert.assertFalse(client.data);
-        Assert.assertTrue(client.dataType);
-    }
+		Assert.assertFalse(client.metadata);
+		Assert.assertTrue(client.data);
+		Assert.assertFalse(client.dataType);
+	}
+
+	@Test
+	public void dataType() throws LightblueException {
+		TestLightblueClient client = new TestLightblueClient();
+		LightblueHystrixClient hystrixClient = new LightblueHystrixClient(client, "group", "command");
+
+		Assert.assertFalse(client.metadata);
+		Assert.assertFalse(client.data);
+		Assert.assertFalse(client.dataType);
+
+		hystrixClient.data(null, Object.class);
+
+		Assert.assertFalse(client.metadata);
+		Assert.assertFalse(client.data);
+		Assert.assertTrue(client.dataType);
+	}
 }
