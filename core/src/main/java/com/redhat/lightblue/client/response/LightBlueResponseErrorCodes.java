1f12dcc26ca28debb01924811e63c538070c839f
Sneha Gunta <sgunta@redhat.com>
2015-09-08 14:13:29 -0400

    pulled exception error code constants into a seperate class

--- core/src/main/java/com/redhat/lightblue/client/response/LightBlueResponseErrorCodes.java
+++ core/src/main/java/com/redhat/lightblue/client/response/LightBlueResponseErrorCodes.java
@@ -1,22 +1,8 @@
 package com.redhat.lightblue.client.response;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.redhat.lightblue.client.model.DataError;
-import com.redhat.lightblue.client.model.Error;
-
-/**
- * Exception thrown when a response from lightblue has errors or data errors.
- * The client can call e.exists(ERR_REST_CRUD_REST_SAVE)
- *
- * @author ykoer
- */
-public class LightblueException extends Exception {
-
-    private static final long serialVersionUID = -5883690817679713469L;
-
-   /* public static final String ERR_NO_OBJECT_TYPE = "NO_OBJECT_TYPE";
+public class LightBlueResponseErrorCodes {
+	
+	public static final String ERR_NO_OBJECT_TYPE = "NO_OBJECT_TYPE";
     public static final String ERR_INVALID_OBJECTTYPE = "INVALID_OBJECTTYPE";
     public static final String ERR_INVALID_FIELD = "INVALID_FIELD";
     public static final String ERR_INVALID_COMPARISON = "INVALID_COMPARISON";
@@ -200,66 +186,6 @@ public class LightblueException extends Exception {
     public static final String ERR_REST_METADATA_NO_VERSION_MATCH = "rest-metadata:NoVersionMatch";
     public static final String ERR_REST_METADATA_CANT_GET_METADATA = "rest-metadata:CantGetMetadata";
     public static final String ERR_REST_METADATA_CANT_GET_PARSER = "rest-metadata:CantGetParser";
-    public static final String ERR_REST_METADATA_CANT_GET_TRANSLATOR = "rest-metadata:CantGetJsonTranslator";*/
+    public static final String ERR_REST_METADATA_CANT_GET_TRANSLATOR = "rest-metadata:CantGetJsonTranslator";
 
-    private LightblueResponse lightblueResponse;
-    private Set<String> errorCodes;
-
-    public LightblueException() {
-        super();
-    }
-
-    public LightblueException(String message, LightblueResponse lightblueResponse) {
-        super(message);
-        this.lightblueResponse = lightblueResponse;
-    }
-
-    public LightblueException(String message, LightblueResponse lightblueResponse, Throwable cause) {
-        super(message, cause);
-        this.lightblueResponse = lightblueResponse;
-    }
-
-    public LightblueResponse getLightblueResponse() {
-        return lightblueResponse;
-    }
-
-    public void setLightblueResponse(LightblueResponse lightblueResponse) {
-        this.lightblueResponse = lightblueResponse;
-    }
-
-    public boolean exists(String errorCode) {
-        return getErrorCodes().contains(errorCode);
-    }
-
-    public Set<String> getErrorCodes() {
-        if (errorCodes == null || lightblueResponse == null) {
-            errorCodes = new HashSet<>();
-        } else {
-            return errorCodes;
-        }
-
-        if (lightblueResponse.getErrors() != null) {
-            for (Error e : lightblueResponse.getErrors()) {
-                errorCodes.add(e.getErrorCode());
-            }
-        }
-        if (lightblueResponse.getDataErrors() != null) {
-            for (DataError de : lightblueResponse.getDataErrors()) {
-                if (de.getErrors() != null) {
-                    for (Error e : de.getErrors()) {
-                        errorCodes.add(e.getErrorCode());
-                    }
-                }
-            }
-        }
-        return errorCodes;
-    }
-
-    @Override
-    public String getMessage() {
-        if (lightblueResponse != null)
-            return super.getMessage() + lightblueResponse.getText();
-        else
-            return super.getMessage();
-    }
 }
