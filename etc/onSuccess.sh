7f1281e4ab00f1e572213b0fee1b0ed16dd1208f
Dennis Crissman <dcrissma@redhat.com>
2016-04-29 12:29:44 -0400

    travis tweak

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk7" ] &
     mvn clean deploy -DskipTests;
 fi
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "DEPLOY JDK8 lightblue-client-integration-test TRAVIS BUILD"
     cd lightblue-client-integration-test
     echo "Current directory is $(pwd)"
