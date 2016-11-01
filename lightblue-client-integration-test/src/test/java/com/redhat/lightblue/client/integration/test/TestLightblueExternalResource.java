e10002862ecccf6ad02876f365bb5b286c23f716
Dennis Crissman <dcrissma@redhat.com>
2016-10-31 21:41:26 -0400

    unit test

--- lightblue-client-integration-test/src/test/java/com/redhat/lightblue/client/integration/test/TestLightblueExternalResource.java
+++ lightblue-client-integration-test/src/test/java/com/redhat/lightblue/client/integration/test/TestLightblueExternalResource.java
@@ -3,21 +3,21 @@ package com.redhat.lightblue.client.integration.test;
 import static com.redhat.lightblue.util.test.AbstractJsonNodeTest.loadJsonNode;
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.redhat.lightblue.client.LightblueClient;
 import com.redhat.lightblue.client.Projection;
 import com.redhat.lightblue.client.integration.test.LightblueExternalResource.LightblueTestMethods;
 import com.redhat.lightblue.client.integration.test.pojo.Country;
 import com.redhat.lightblue.client.request.data.DataInsertRequest;
 import com.redhat.lightblue.client.response.LightblueDataResponse;
+import com.redhat.lightblue.rest.integration.FakeIdentityManager;
 
 public class TestLightblueExternalResource {
-
-    private LightblueClient client;
 
     @ClassRule
     public static LightblueExternalResource lightblue = new LightblueExternalResource(new LightblueTestMethods() {
@@ -30,8 +30,12 @@ public class TestLightblueExternalResource {
     });
 
     @Before
-    public void before() {
-        client = lightblue.getLightblueClient();
+    public void before() throws IOException {
+        lightblue.getControllerInstance().cleanupMongoCollections("country");
+
+        if (lightblue.getControllerInstance().getIdentityManager() != null) {
+            lightblue.changeIdentityManager(null);
+        }
     }
 
     @Test
@@ -45,7 +49,26 @@ public class TestLightblueExternalResource {
         insert.create(c);
         insert.returns(Projection.includeFieldRecursively("*"));
 
-        LightblueDataResponse insertResponse = client.data(insert);
+        LightblueDataResponse insertResponse = lightblue.getLightblueClient().data(insert);
+
+        assertEquals(1, insertResponse.parseModifiedCount());
+    }
+
+    @Test
+    public void testWithIdentityManager() throws Exception {
+        lightblue.changeIdentityManager(
+                new FakeIdentityManager().add("fakeuser", "fakepassword"));
+
+        Country c = new Country();
+        c.setName("Poland");
+        c.setIso2Code("PL");
+        c.setIso3Code("POL");
+
+        DataInsertRequest insert = new DataInsertRequest(Country.objectType, Country.objectVersion);
+        insert.create(c);
+        insert.returns(Projection.includeFieldRecursively("*"));
+
+        LightblueDataResponse insertResponse = lightblue.getLightblueClient().data(insert);
 
         assertEquals(1, insertResponse.parseModifiedCount());
     }
