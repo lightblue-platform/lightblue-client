b17cfa8bd4efab60292a45d4927acf263f97f2bd
Derek Haynes <dhaynes@redhat.com>
2021-09-16 10:19:41 +0200

    Remove TravisCI config

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$BRANCH" == "master" ] && [ "$JDK_VERSION" == "openjdk7" ] && [ "$PULL_REQUEST" == "false" ]; then
-    echo "DEPLOY MASTER TRAVIS BUILD"
+    echo "DEPLOY MASTER BUILD"
     echo "Current directory is $(pwd)"
     mvn clean deploy -DskipTests;
 fi
 
 if [ "$BRANCH" == "master" ] && [ "$JDK_VERSION" == "oraclejdk8" ] && [ "$PULL_REQUEST" == "false" ]; then
-    echo "DEPLOY JDK8 lightblue-client-integration-test TRAVIS BUILD"
+    echo "DEPLOY JDK8 lightblue-client-integration-test BUILD"
     cd lightblue-client-integration-test
     echo "Current directory is $(pwd)"
     mvn clean deploy -DskipTests;
